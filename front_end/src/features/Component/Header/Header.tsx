bf80aab93ff1713e56acc2b1f03d3133d5a3fe0a
thuhoai1810 <dinhhoai18101998@gmail.com>
2023-02-09 18:07:15 +0700

    #1 Update color css

--- front_end/src/features/Component/Header/Header.tsx
+++ front_end/src/features/Component/Header/Header.tsx
@@ -2,7 +2,13 @@ import React  from "react";
 import  styles from './Header.module.scss'
 const Header = () =>{
  return (
-     <div className={styles.container}>Header</div>
+     <div className={styles.container}>
+      <div>
+       <p>
+
+       </p>
+      </div>
+     </div>
  )
 }
  export  default  Header
